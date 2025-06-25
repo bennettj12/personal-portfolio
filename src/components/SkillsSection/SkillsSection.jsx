7fa58055d2dc268bb67fd863aee0d6ec3a3a28d3
Bennett Johnson <bennettjohnson0@gmail.com>
2025-06-24 21:02:27 -0500

    adding content to about page

--- src/components/SkillsSection/SkillsSection.jsx
+++ src/components/SkillsSection/SkillsSection.jsx
@@ -1,10 +1,28 @@
+import Divider from '../Divider/divider.jsx'
 import styles from './SkillsSection.module.scss'
-
+import { SlGraduation, SlLayers, SlPencil } from "react-icons/sl";
 export default function SkillsSection() {
     return(
         <div className={styles.skillsContainer}>
-            <h2 className={styles.headline}>About Me</h2>
-
+            <div className={styles.skillsBox}> 
+                <SlGraduation className={styles.skillsIcon} />
+                <h3>Education</h3>
+                <Divider animate={false}/>
+                <p>
+                <span className={styles.degree}>B.S. Computer Science</span><br/>
+                <span className={styles.school}>University of Illinois at Springfield</span>
+                </p>
+            </div>
+            <div className={styles.skillsBox}> 
+                <SlLayers className={styles.skillsIcon} />
+                <h3>Skills</h3>
+                <Divider reverse animate={false}/>
+            </div>
+            <div className={styles.skillsBox}> 
+                <SlPencil className={styles.skillsIcon} />
+                <h3>Etc...</h3>
+                <Divider animate={false}/>
+            </div>
         </div>
     )
 }
